53df861793d0eb26d5001e6c5e7e9b533197d156
JoaoGamaa <joaohlgama@gmail.com>
2026-03-27 03:19:55 -0300

    adicionar senha

--- src/main/java/viewer/FrmPrincipal.java
+++ src/main/java/viewer/FrmPrincipal.java
@@ -287,9 +287,30 @@ private void initEventos() {
     }
 }
     
-    
-    /**
-     * @param args the command line argumen
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(() -> new FrmPrincipal().setVisible(true));
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ButaoAgenda;
     private javax.swing.JRadioButtonMenuItem MenuMotif;
